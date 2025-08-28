78f5b4ab1ba614fced242410f58a7e4ef0b7e64e
dazel11 <damianzelada7@gmail.com>
2025-08-28 09:07:45 -0400

    Add files via upload

---
+++ informacion.py
@@ -0,0 +1,9 @@
+nombre = "Damian Zelada"
+edad = 18
+lugar_estudio = "Instituto Profesional de Comercio"
+
+
+print("=== Información Personal ===")
+print(f"Nombre: {nombre}")
+print(f"Edad: {edad} años")
+print(f"Lugar de estudio: {lugar_estudio}")
